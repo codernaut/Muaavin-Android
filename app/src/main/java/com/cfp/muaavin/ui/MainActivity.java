b0cba32ea1bc88c8d86a2f7abbb9a9969b361493
Gul Zaib <gul.zaib@lmkr.com>
2016-10-23 12:58:40 +0500

    added hash key generation to log

--- app/src/main/java/com/cfp/muaavin/ui/MainActivity.java
+++ app/src/main/java/com/cfp/muaavin/ui/MainActivity.java
@@ -2,8 +2,13 @@ package com.cfp.muaavin.ui;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,6 +25,8 @@ import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 
@@ -35,7 +42,20 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
 
         context = this;
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.tooba.facebookapplication2",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
 
+        } catch (NoSuchAlgorithmException e) {
+
+        }
         FacebookSdk.sdkInitialize(getApplicationContext());
 
         setContentView(R.layout.activity_main);
